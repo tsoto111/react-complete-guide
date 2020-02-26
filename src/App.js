4b8b7b98c43186c036d32da010258865871f9995
Tavo Soto <tavo@newmedio.com>
2020-02-26 12:06:12 -0600

    Understanding JSX

--- src/App.js
+++ src/App.js
@@ -6,8 +6,11 @@ class App extends Component {
     return (
       <div className="App">
 		  <h1>Hi, I am a React App</h1>
+		  <p>This is really working!</p>
       </div>
-    );
+	);
+	// This is the exact same thing as above. This is just an example of what is happening under the hood...
+	// return React.createElement('div', null, React.createElement('h1', null, 'Does this work now?'));
   }
 }
 
