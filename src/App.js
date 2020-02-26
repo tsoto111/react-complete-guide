f5f1e8a9b50bdc212eee39f081d5ff38698e570c
Tavo Soto <tavo@newmedio.com>
2020-02-26 14:33:23 -0600

    Example of reusing static component

--- src/App.js
+++ src/App.js
@@ -9,6 +9,8 @@ class App extends Component {
 		  <h1>Hi, I am a React App</h1>
 		  <p>This is really working!</p>
 		  <Person/>
+		  <Person/>
+		  <Person/>
       </div>
 	);
 	// This is the exact same thing as above. This is just an example of what is happening under the hood...
