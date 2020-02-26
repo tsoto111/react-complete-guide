895ecf3d768b1f6bcd425581ec37dfc5b57bdb3e
Tavo Soto <tavo@newmedio.com>
2020-02-26 12:49:53 -0600

    Creating and using my first static or function based component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
   render() {
@@ -7,6 +8,7 @@ class App extends Component {
       <div className="App">
 		  <h1>Hi, I am a React App</h1>
 		  <p>This is really working!</p>
+		  <Person/>
       </div>
 	);
 	// This is the exact same thing as above. This is just an example of what is happening under the hood...
