829614556e704547e4f80ef57e65b91d4f2fbd1a
Tavo Soto <tavo@newmedio.com>
2020-02-26 14:43:35 -0600

    Adding logic inside of JSX markup

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -3,7 +3,9 @@ import React from 'react';
 // ES6's way of creating a function. No use of `this`...
 const Person = () => {
 	return (
-		<p>I'm a Person!</p>
+	<p>
+		I'm a Person and I am {Math.floor(Math.random() * 30)} years old!
+	</p>
 	);
 }
 
