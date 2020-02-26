b78a9f48813f436f7e9654efcb1fb9e340cd49ca
Tavo Soto <tavo@newmedio.com>
2020-02-26 17:12:53 -0600

    Setting up a state to turn our static component into a reactive one

--- src/App.js
+++ src/App.js
@@ -3,14 +3,24 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+
+  state = {
+    persons: [
+      { name: 'Tavo', age: 28 }, 
+      { name: 'Scarlett', age: 31 }, 
+      { name: 'Parker', age: 2 }
+    ]
+  }
+
   render() {
     return (
       <div className="App">
 		  <h1>Hi, I am a React App</h1>
 		  <p>This is really working!</p>
-		  <Person name="Tavo" age="28" />
-		  <Person name="Scarlett" age="31">My Hobbies: Singing, and Piano</Person>
-		  <Person name="Parker" age="2" />
+      <button>Switch Name</button>
+		  <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+		  <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My Hobbies: Singing, and Piano</Person>
+		  <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
       </div>
 	);
 	// This is the exact same thing as above. This is just an example of what is happening under the hood...
