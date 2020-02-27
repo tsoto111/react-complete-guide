d106b8f1371a32c512d348b9ac2dc7218d1c6721
Tavo Soto <tavo@newmedio.com>
2020-02-26 18:45:36 -0600

    Converting our class based stateful component into a stateful function based component using useState() Hook

--- src/App.js
+++ src/App.js
@@ -1,39 +1,44 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Person from './Person/Person';
 
-class App extends Component {
+// Class based component which extends Component
+// class App extends Component {
 
-  state = {
-    persons: [
-      { name: 'Tavo', age: 28 }, 
-      { name: 'Scarlett', age: 31 }, 
-      { name: 'Parker', age: 2 }
-    ]
-  }
+// Function based component
+const app = (props) => {
 
-  switchNameHandler = () => {
-    this.setState({persons: [
-      { name: 'Gustavo', age: 28 }, 
-      { name: 'Scarlett', age: 31 }, 
-      { name: 'Parker', age: 1 }
+  // useState Hook method
+  const [personsState, setPersonsState] = useState({
+      persons: [
+        { name: 'Tavo', age: 28 }, 
+        { name: 'Scarlett', age: 31 }, 
+        { name: 'Parker', age: 2 }
+      ]
+  });
+
+  const switchNameHandler = () => {
+    // This will not merge, but replace the old state!!!
+    setPersonsState({
+      persons: [
+        {name: 'Gustavo', age: '28'},
+        {name: 'HyeRee', age: '31'},
+        {name: 'Yejun', age: '1'}
     ]});
   }
-
-  render() {
-    return (
-      <div className="App">
+  
+  return (
+    <div className="App">
 		  <h1>Hi, I am a React App</h1>
 		  <p>This is really working!</p>
-      <button onClick={this.switchNameHandler}>Switch Name</button>
-		  <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
-		  <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My Hobbies: Singing, and Piano</Person>
-		  <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
-      </div>
+      <button onClick={switchNameHandler}>Switch Name</button>
+		  <Person name={personsState.persons[0].name} age={personsState.persons[0].age} />
+		  <Person name={personsState.persons[1].name} age={personsState.persons[1].age}>My Hobbies: Singing, and Piano</Person>
+		  <Person name={personsState.persons[2].name} age={personsState.persons[2].age} />
+    </div>
 	);
 	// This is the exact same thing as above. This is just an example of what is happening under the hood...
 	// return React.createElement('div', null, React.createElement('h1', null, 'Does this work now?'));
-  }
 }
 
-export default App;
+export default app;
